7c042fe45ca3b72f4302ca7b7d89ce4756298f89
mohamedvip28 <mohamed.benmarzouk@online-vip-consulting.com>
2025-09-25 15:55:55 +0100

    refactor(api): restructure database items and create backup
    - Replace cluttered items array in db.js with organized folders (Documents, Images, Projects) and sample files
    - Add new db.js.backup file with structured data and comments for reference
    - Minor update to package.json start script for consistency
    
    BREAKING CHANGE: Database structure has been completely reorganized, potentially affecting any code relying on the old item IDs or structure.

--- api/db.js
+++ api/db.js
@@ -1,211 +1,17 @@
 module.exports = {
   "items": [
     {
-      "id": "92ac8d55-b00e-497a-8205-14b3ec37ce1e",
+      "id": "folder-documents",
       "parentId": null,
-      "name": "arrow-up-circle.svg",
-      "folder": false,
-      "filePath": "2a296d62503331d4dac609cab76043c6",
-      "size": 357,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-22T22:19:15.948Z",
-      "modification": "2025-09-22T22:19:15.948Z"
-    },
-    {
-      "id": "53857f8d-1ec5-4401-9f3f-044b50b57b21",
-      "parentId": null,
-      "name": "new-folder",
+      "name": "Documents",
       "folder": true,
-      "creation": "2025-09-22T22:20:26.782Z",
-      "modification": "2025-09-22T22:20:26.782Z"
+      "creation": "2025-08-01T10:00:00.000Z",
+      "modification": "2025-09-24T15:30:00.000Z"
     },
     {
-      "id": "4521a002-1bc0-411c-b9da-f92bf90f5926",
-      "parentId": null,
-      "name": "rotate-ccw.svg",
-      "folder": false,
-      "filePath": "6b6d5a9a9a76cc4d5775a00d56a77148",
-      "size": 317,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-22T22:24:05.334Z",
-      "modification": "2025-09-22T22:24:05.334Z"
-    },
-    {
-      "id": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "parentId": null,
-      "name": "new",
-      "folder": true,
-      "creation": "2025-09-23T13:04:22.024Z",
-      "modification": "2025-09-23T13:04:22.024Z"
-    },
-    {
-      "id": "278067db-ba3f-4bed-a34f-5ebb20b2a73e",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "ppp",
-      "folder": true,
-      "creation": "2025-09-23T13:05:37.531Z",
-      "modification": "2025-09-23T13:05:37.531Z"
-    },
-    {
-      "id": "88e412dd-8065-493b-bc8b-9bde5a0e53e1",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "Psg-Logo-PNG-File.png",
-      "folder": false,
-      "filePath": "9a83419f13429404db06b2c5f5e278c7",
-      "size": 118995,
-      "mimeType": "image/png",
-      "creation": "2025-09-23T13:05:55.820Z",
-      "modification": "2025-09-23T13:05:55.820Z"
-    },
-    {
-      "id": "906ad158-a200-495d-a563-4f9019e6cbb4",
-      "parentId": null,
-      "name": "etude_catalogue (8).pdf",
-      "folder": false,
-      "filePath": "a55aae5acfd1e2046072b21596004fd6",
-      "size": 3683668,
-      "mimeType": "application/pdf",
-      "creation": "2025-09-23T13:19:37.223Z",
-      "modification": "2025-09-23T13:19:37.223Z"
-    },
-    {
-      "id": "e6308151-1e44-445f-85dd-aaa9d9910742",
-      "parentId": null,
-      "name": "new-folder-2",
-      "folder": true,
-      "creation": "2025-09-23T15:09:05.324Z",
-      "modification": "2025-09-25T13:11:09.214Z"
-    },
-    {
-      "id": "d7d7dd73-322c-49e6-9684-66845db2083e",
-      "parentId": null,
-      "name": "decouverte_270120250919 (2).pdf",
-      "folder": false,
-      "filePath": "8f1300c6c4b241020534ea5efb35085c",
-      "size": 267585,
-      "mimeType": "application/pdf",
-      "creation": "2025-09-23T16:50:09.085Z",
-      "modification": "2025-09-23T16:50:09.085Z"
-    },
-    {
-      "id": "2e34af97-0f4f-4396-af4a-746eb4c70b9f",
-      "parentId": null,
-      "name": "decouverte_270120250919 (1).pdf",
-      "folder": false,
-      "filePath": "0e886c5b556cbe2ea007a3cf0ac69f70",
-      "size": 267585,
-      "mimeType": "application/pdf",
-      "creation": "2025-09-23T16:50:09.085Z",
-      "modification": "2025-09-23T16:50:09.085Z"
-    },
-    {
-      "id": "dc5c8894-f1ba-4867-9f12-e57de03e9e6c",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "fonts",
-      "folder": true,
-      "creation": "2025-09-24T18:34:03.983Z",
-      "modification": "2025-09-24T18:34:03.983Z"
-    },
-    {
-      "id": "b5be60de-46af-4d37-8624-29407e6b3883",
-      "parentId": "dc5c8894-f1ba-4867-9f12-e57de03e9e6c",
-      "name": "feather.eot",
-      "folder": false,
-      "filePath": "68ce6fa43200cf2395550b474af157c3",
-      "size": 62084,
-      "mimeType": "application/octet-stream",
-      "creation": "2025-09-24T18:34:04.013Z",
-      "modification": "2025-09-24T18:34:04.013Z"
-    },
-    {
-      "id": "4e8919b0-1461-48b3-a368-8e49d8b417e3",
-      "parentId": "dc5c8894-f1ba-4867-9f12-e57de03e9e6c",
-      "name": "feather.svg",
-      "folder": false,
-      "filePath": "3a28383e6a46f2a6be08b9279e30fd5f",
-      "size": 214775,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T18:34:04.013Z",
-      "modification": "2025-09-24T18:34:04.013Z"
-    },
-    {
-      "id": "fe2c5e7c-93af-4df5-9828-580812b0d62d",
-      "parentId": "dc5c8894-f1ba-4867-9f12-e57de03e9e6c",
-      "name": "feather.ttf",
-      "folder": false,
-      "filePath": "872d42c41700d2c6620e870e1130cd24",
-      "size": 61920,
-      "mimeType": "application/octet-stream",
-      "creation": "2025-09-24T18:34:04.013Z",
-      "modification": "2025-09-24T18:34:04.013Z"
-    },
-    {
-      "id": "f46eee9e-ef19-46e2-8dad-35b7862e0f36",
-      "parentId": "dc5c8894-f1ba-4867-9f12-e57de03e9e6c",
-      "name": "feather.woff",
-      "folder": false,
-      "filePath": "9931438684248fd236c2adc3cc7c7053",
-      "size": 29500,
-      "mimeType": "application/font-woff",
-      "creation": "2025-09-24T18:34:04.013Z",
-      "modification": "2025-09-24T18:34:04.013Z"
-    },
-    {
-      "id": "a1467d9a-9f3d-4835-bd7d-37b2ff1bec22",
-      "parentId": "278067db-ba3f-4bed-a34f-5ebb20b2a73e",
-      "name": "ytjytjytjyt",
-      "folder": true,
-      "creation": "2025-09-24T20:27:38.025Z",
-      "modification": "2025-09-24T20:27:38.025Z"
-    },
-    {
-      "id": "31ea3191-f02b-4caa-8da9-b2a961b6a8c4",
-      "parentId": null,
-      "name": "arrow-up-circle (3).svg",
-      "folder": false,
-      "filePath": "4c90342357a390d3ad6702e4a50bc60a",
-      "size": 357,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T20:53:05.968Z",
-      "modification": "2025-09-24T20:53:05.968Z"
-    },
-    {
-      "id": "34c557de-0779-46b6-a2ce-88bb30e88869",
-      "parentId": null,
-      "name": "trash-2.svg",
-      "folder": false,
-      "filePath": "eac1a6ee17bd6bbfb630979927b9f10e",
-      "size": 448,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T20:53:50.064Z",
-      "modification": "2025-09-24T20:53:50.064Z"
-    },
-    {
-      "id": "30ca5ac9-a797-45ec-9202-9e074549cf6d",
-      "parentId": "53857f8d-1ec5-4401-9f3f-044b50b57b21",
-      "name": "refresh-cw (2).svg",
-      "folder": false,
-      "filePath": "805de6b791594945f507493cd453d063",
-      "size": 400,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T20:54:44.836Z",
-      "modification": "2025-09-24T20:54:44.836Z"
-    },
-    {
-      "id": "aaee78eb-24fe-4a4c-917f-a17f043ca6fa",
-      "parentId": "53857f8d-1ec5-4401-9f3f-044b50b57b21",
-      "name": "arrow-up-circle (2).svg",
-      "folder": false,
-      "filePath": "8cbaddccdbbfe2b49cd63e750de92494",
-      "size": 357,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T20:57:13.810Z",
-      "modification": "2025-09-24T20:57:13.810Z"
-    },
-    {
-      "id": "de0e6448-d03d-4c16-b469-caaca33904d2",
-      "parentId": "53857f8d-1ec5-4401-9f3f-044b50b57b21",
-      "name": "document (32).pdf",
+      "id": "file-pdf-1",
+      "parentId": "folder-documents",
+      "name": "Important Document.pdf",
       "folder": false,
       "filePath": "326ea1b164c80e67c54a1b7c8c13d381",
       "size": 1437637,
@@ -214,110 +20,28 @@ module.exports = {
       "modification": "2025-09-24T20:57:38.923Z"
     },
     {
-      "id": "1a2eb398-d9d0-412d-9ce8-279e790e5135",
-      "parentId": "53857f8d-1ec5-4401-9f3f-044b50b57b21",
-      "name": "feather.svg",
-      "folder": false,
-      "filePath": "cd02a754c48aa52e0ba4ace636574986",
-      "size": 214775,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T21:01:53.562Z",
-      "modification": "2025-09-24T21:01:53.562Z"
-    },
-    {
-      "id": "3fd7f447-d8b2-49f5-83b5-1d5ce24a4757",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "feather.svg",
-      "folder": false,
-      "filePath": "5ba0442c837e524dcdb1dd798df12229",
-      "size": 214775,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T21:02:13.210Z",
-      "modification": "2025-09-24T21:02:13.210Z"
-    },
-    {
-      "id": "df6eeb3f-7bc2-4bd8-8bbf-91caa704a6f8",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "rn_image_picker_lib_temp_c381c557-e17d-4575-bfa3-2aca97252e5c.jpg",
-      "folder": false,
-      "filePath": "c0c3c8d4101023d941308f7c9b577c57",
-      "size": 22700,
-      "mimeType": "image/jpeg",
-      "creation": "2025-09-24T21:03:04.631Z",
-      "modification": "2025-09-24T21:03:04.631Z"
-    },
-    {
-      "id": "177cad6b-42f9-4bd8-abbd-ce94fd6aac22",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "email.svg",
-      "folder": false,
-      "filePath": "7425580eb357c92c43a6e384e6256b64",
-      "size": 31391,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T21:08:09.048Z",
-      "modification": "2025-09-24T21:08:09.048Z"
-    },
-    {
-      "id": "81dfd9df-3914-4d46-aaf5-6ccbf1c1db8c",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "sales.svg",
-      "folder": false,
-      "filePath": "281e2271f4ad03066e17d029cc8a011c",
-      "size": 35116,
-      "mimeType": "image/svg+xml",
-      "creation": "2025-09-24T21:09:13.213Z",
-      "modification": "2025-09-24T21:09:13.213Z"
-    },
-    {
-      "id": "a3ea5db6-06cc-43f8-b611-e9ebff39c099",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "small",
-      "folder": true,
-      "creation": "2025-09-24T21:09:32.741Z",
-      "modification": "2025-09-24T21:09:32.741Z"
-    },
-    {
-      "id": "017112e1-9970-414d-b2c0-cc71e451461a",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "user-uploads",
-      "folder": true,
-      "creation": "2025-09-24T23:52:28.939Z",
-      "modification": "2025-09-24T23:52:28.939Z"
-    },
-    {
-      "id": "bc795ddb-bf85-45d9-8c18-42f6371ce21e",
-      "parentId": "f7196f11-a66a-4150-bcb4-7919cab91b22",
-      "name": "post-media",
-      "folder": true,
-      "creation": "2025-09-25T00:06:05.755Z",
-      "modification": "2025-09-25T00:06:05.755Z"
-    },
-    {
-      "id": "0602428f-e7b6-4a72-bd2b-93de81be896f",
-      "parentId": "bc795ddb-bf85-45d9-8c18-42f6371ce21e",
-      "name": "2.jpg",
-      "folder": false,
-      "filePath": "e9a09cfd90be04615f4b20026fd2b684",
-      "size": 148242,
-      "mimeType": "image/jpeg",
-      "creation": "2025-09-25T00:06:05.804Z",
-      "modification": "2025-09-25T00:06:05.804Z"
-    },
-    {
-      "id": "bdb74158-e84a-4e51-9837-783089a765b0",
-      "parentId": "bc795ddb-bf85-45d9-8c18-42f6371ce21e",
-      "name": "25.jpg",
-      "folder": false,
-      "filePath": "ea7fe21b468f97412160ed5a4409d87c",
-      "size": 524458,
-      "mimeType": "image/jpeg",
-      "creation": "2025-09-25T00:06:05.804Z",
-      "modification": "2025-09-25T00:06:05.804Z"
-    },
-    {
-      "id": "8fe8c665-37d6-47bf-a418-450d7d96feb0",
+      "id": "folder-images",
       "parentId": null,
-      "name": "2.jpg",
+      "name": "Images",
+      "folder": true,
+      "creation": "2025-08-01T10:00:00.000Z",
+      "modification": "2025-09-24T16:45:00.000Z"
+    },
+    {
+      "id": "file-image-1",
+      "parentId": "folder-images",
+      "name": "Project Logo.svg",
+      "folder": false,
+      "filePath": "2a296d62503331d4dac609cab76043c6",
+      "size": 357,
+      "mimeType": "image/svg+xml",
+      "creation": "2025-09-22T22:19:15.948Z",
+      "modification": "2025-09-22T22:19:15.948Z"
+    },
+    {
+      "id": "file-image-2",
+      "parentId": "folder-images",
+      "name": "Team Photo.jpg",
       "folder": false,
       "filePath": "027f8d0f9f42211f87dc98426f12aa32",
       "size": 148242,
@@ -326,59 +50,86 @@ module.exports = {
       "modification": "2025-09-25T00:38:28.174Z"
     },
     {
-      "id": "2010604d-1542-4325-a324-064be1d3cf7f",
-      "parentId": "e6308151-1e44-445f-85dd-aaa9d9910742",
-      "name": "prize_0.png",
-      "folder": false,
-      "filePath": "0c81a1b338dfe15ed3670a0c6348ea72",
-      "size": 10273,
-      "mimeType": "image/png",
-      "creation": "2025-09-25T12:57:56.043Z",
-      "modification": "2025-09-25T12:57:56.043Z"
-    },
-    {
-      "id": "cc862c92-d697-40c3-8af7-3574665e4fa2",
+      "id": "folder-projects",
       "parentId": null,
-      "name": "prize_011.png",
-      "folder": false,
-      "filePath": "18a89d19f6a19fb48aecdfd6f7439875",
-      "size": 10273,
-      "mimeType": "image/png",
-      "creation": "2025-09-25T13:02:26.802Z",
-      "modification": "2025-09-25T13:11:22.095Z"
+      "name": "Projects",
+      "folder": true,
+      "creation": "2025-08-15T09:30:00.000Z",
+      "modification": "2025-09-24T14:20:00.000Z"
     },
     {
-      "id": "c1a16886-e94c-4189-a624-7576b53c96f4",
-      "parentId": "e6308151-1e44-445f-85dd-aaa9d9910742",
-      "name": "hangUpSupIcon.png",
+      "id": "file-large-doc",
+      "parentId": "folder-projects",
+      "name": "Project Documentation.pdf",
       "folder": false,
-      "filePath": "e28a08d50d732e7899865596dba4f959",
-      "size": 1784,
-      "mimeType": "image/png",
-      "creation": "2025-09-25T13:02:35.557Z",
-      "modification": "2025-09-25T13:02:35.557Z"
+      "filePath": "a55aae5acfd1e2046072b21596004fd6",
+      "size": 3683668,
+      "mimeType": "application/pdf",
+      "creation": "2025-09-23T13:19:37.223Z",
+      "modification": "2025-09-23T13:19:37.223Z"
     },
     {
-      "id": "d45b17ba-2cfc-4bc5-b951-92160c1711f8",
+      "id": "e3d0f0e1-75a5-405e-a9ca-53caeacdc90f",
+      "parentId": "folder-documents",
+      "name": "document100).pdf",
+      "folder": false,
+      "filePath": "e4499d903ab76ef609fb0773a6d37ec2",
+      "size": 11713880,
+      "mimeType": "application/pdf",
+      "creation": "2025-09-25T14:43:25.268Z",
+      "modification": "2025-09-25T14:43:50.362Z"
+    },
+    {
+      "id": "6f5a04cb-78db-4c72-8117-d9e3e3a30e10",
       "parentId": null,
-      "name": "done----.png",
+      "name": "prize_1.png",
       "folder": false,
-      "filePath": "3052f6e39ddb2c94858617a1d57ea35c",
-      "size": 4489,
+      "filePath": "81a793870ffe489c88acc62a43978b8f",
+      "size": 11030,
       "mimeType": "image/png",
-      "creation": "2025-09-25T13:10:35.946Z",
-      "modification": "2025-09-25T13:11:33.030Z"
+      "creation": "2025-09-25T14:44:08.799Z",
+      "modification": "2025-09-25T14:44:08.799Z"
     },
     {
-      "id": "85246b4b-7082-4232-b593-3eb47b95d979",
+      "id": "a419c197-5e66-4b02-b104-ad0ec1a1fff2",
       "parentId": null,
-      "name": "toggleDownIcon.png",
+      "name": "timeline.jpg",
       "folder": false,
-      "filePath": "d6b4beffb934c011e316719c71f48926",
-      "size": 551,
-      "mimeType": "image/png",
-      "creation": "2025-09-25T13:25:31.295Z",
-      "modification": "2025-09-25T13:25:31.295Z"
+      "filePath": "d064bd82760fff0640f8cff6584931f5",
+      "size": 538459,
+      "mimeType": "image/jpeg",
+      "creation": "2025-09-25T14:46:14.999Z",
+      "modification": "2025-09-25T14:46:14.999Z"
+    },
+    {
+      "id": "5c646565-e132-4aca-a07f-3657b100c6f7",
+      "parentId": null,
+      "name": "post-media",
+      "folder": true,
+      "creation": "2025-09-25T14:46:31.000Z",
+      "modification": "2025-09-25T14:46:31.000Z"
+    },
+    {
+      "id": "3ead732e-b91c-4cf6-bfc5-494220ae4369",
+      "parentId": "5c646565-e132-4aca-a07f-3657b100c6f7",
+      "name": "2.jpg",
+      "folder": false,
+      "filePath": "a9bcc50328b4c953c426728fce615cfb",
+      "size": 148242,
+      "mimeType": "image/jpeg",
+      "creation": "2025-09-25T14:46:31.030Z",
+      "modification": "2025-09-25T14:46:31.030Z"
+    },
+    {
+      "id": "11244163-1653-4656-a735-f177706a2d8f",
+      "parentId": "5c646565-e132-4aca-a07f-3657b100c6f7",
+      "name": "25.jpg",
+      "folder": false,
+      "filePath": "963452bbe26ee936300362ba1d5200e8",
+      "size": 524458,
+      "mimeType": "image/jpeg",
+      "creation": "2025-09-25T14:46:31.030Z",
+      "modification": "2025-09-25T14:46:31.030Z"
     }
   ]
 };
